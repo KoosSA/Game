c193b39bca620c49a686294457fd105768809e75
KoosSA <kkposbus@gmail.com>
2023-04-27 14:43:31 +0200

    Added material to mesh

--- Game-Client/src/main/java/client/rendering/objects/Mesh.java
+++ Game-Client/src/main/java/client/rendering/objects/Mesh.java
@@ -1,21 +1,27 @@
 package client.rendering.objects;
 
+import client.rendering.materials.Material;
+
 public class Mesh {
 	
 	private float[] vertices, normals,  texCoords;
 	private int[] indices;
+	private Material material;
 
 	
 
-	public Mesh(float[] vertices, float[] normals, float[] texCoords, int[] indices) {
+	public Mesh(float[] vertices, float[] normals, float[] texCoords, int[] indices, Material material) {
 		this.vertices = vertices;
 		this.normals = normals;
 		this.texCoords = texCoords;
 		this.indices = indices;
+		this.material = material;
 	}
 
-
-
+	public Material getMaterial() {
+		return material;
+	}
+	
 	public float[] getVertices() {
 		return vertices;
 	}
