72483b8e5577a5477fb927d44491219609be8cdd
KoosSA <kkposbus@gmail.com>
2023-04-30 12:02:19 +0200

    Addded transform to model
    Might change later to entity or instance list/map

--- Game-Client/src/main/java/client/rendering/objects/Model.java
+++ Game-Client/src/main/java/client/rendering/objects/Model.java
@@ -2,16 +2,24 @@ package client.rendering.objects;
 
 import java.util.List;
 
+import client.rendering.utils.Transform;
+
 public class Model {
 	
 	private List<Mesh> meshes;
+	private Transform transform;
 	
 	public Model(List<Mesh> meshes) {
 		this.meshes = meshes;
+		transform = new Transform();
 	}
 
 	public List<Mesh> getMeshes() {
 		return meshes;
+	}
+	
+	public Transform getTransform() {
+		return transform;
 	}
 
 }
