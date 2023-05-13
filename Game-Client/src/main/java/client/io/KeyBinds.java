01c342708ea33cb27ceed94cf20e809c5e16b194
KoosSA <kkposbus@gmail.com>
2023-05-13 20:52:26 +0200

    Added basic movement key bindings

---
+++ Game-Client/src/main/java/client/io/KeyBinds.java
@@ -0,0 +1,15 @@
+package client.io;
+
+import org.lwjgl.glfw.GLFW;
+
+public class KeyBinds {
+
+	public static final int WALK_FORWARD = GLFW.GLFW_KEY_W;
+	public static final int WALK_BACK = GLFW.GLFW_KEY_S;
+	public static final int WALK_LEFT = GLFW.GLFW_KEY_A;
+	public static final int WALK_RIGHT = GLFW.GLFW_KEY_D;
+	public static final int JUMP = GLFW.GLFW_KEY_SPACE;
+	public static final int CROUCH = GLFW.GLFW_KEY_LEFT_CONTROL;
+	public static final int SPRINT = GLFW.GLFW_KEY_LEFT_SHIFT;
+
+}
