6155b27534064d6b206fa0cc4e6f795cb50161d8
KoosSA <kkposbus@gmail.com>
2023-05-13 22:03:18 +0200

    Use new 3dModel for testing

--- Game-Client/src/main/java/client/Client.java
+++ Game-Client/src/main/java/client/Client.java
@@ -1,13 +1,12 @@
 package client;
 
 import client.io.input.InputStates;
-import client.io.input.receivers.handlers.IKeyInputHandler;
 import client.logic.BaseGameLoop;
+import client.rendering.cameras.FirstPersonCamera;
 import client.rendering.renderers.StaticRenderer;
-import client.rendering.utils.RenderManager;
-import client.utils.Registries;
+import client.utils.registries.Registries;
 
-public class Client extends BaseGameLoop implements IKeyInputHandler {
+public class Client extends BaseGameLoop {
 	
 	public static void main(String[] args) {
 		//Connection c = new Connection("Koos");
@@ -22,58 +21,36 @@ public class Client extends BaseGameLoop implements IKeyInputHandler {
 
 	@Override
 	protected void init() {
-		registerInputHandler(InputStates.GAME);
 		//input.setInputReceiver(InputStates.NONE);
-		gui.loadXML("test.xml");
-		gui.loadXML("hud.xml");
-		gui.loadXML("inv.xml");
+//		gui.loadXML("test.xml");
+//		gui.loadXML("hud.xml");
+//		gui.loadXML("inv.xml");
 		
-		//gui.show("start");
-		//gui.show("hud");
-		gui.show("inv");
 		
-		//ModelLoader ml = new ModelLoader();
-		
-		//ml.loadModel("plane.fbx");
 		input.setInputReceiver(InputStates.GAME);
 		
-		renderer = new StaticRenderer(null);
+		renderer = new StaticRenderer(new FirstPersonCamera());
 		
-		RenderManager.getStaticModels().add(Registries.Models.getModel("plane.fbx"));
-		Registries.Models.getModel("plane.fbx").getTransform().setScale(1000, 1000, 1);
+		Registries.Models.getStaticModel("t.fbx");
+		Registries.Models.getStaticModel("t.fbx").getTransform().setScale(1f, 1, 1);
+		Registries.Models.getStaticModel("t.fbx").getTransform().move(0, 0, 0f);
+//		Registries.Models.getStaticModel("uc_uv_sphere.fbx").getTransform().setScale(0.5f, 0.5f, 1f);
+//		Registries.Models.getStaticModel("uc_uv_sphere.fbx").getTransform().move(0, 0, 5f);
 	}
 
 	@Override
 	protected void update(float delta) {
 		//HUDScreenController.update(delta);
-		
+		//Registries.Models.getStaticModel("monkey_ut.fbx").getTransform().turn(0,1,0);
+//		Registries.Models.getStaticModel("t.fbx").getTransform().turn(0,1,0);
+		//Registries.Models.getStaticModel("t.fbx").getTransform().move(-0.1f, 0, -0);
+		//Registries.Models.getStaticModel("t.fbx").getTransform().getRotation().identity();
+//		Registries.Models.getStaticModel("tsc.fbx").getTransform().setScale(1f, 1f, 1f);
 	}
 
 	@Override
 	protected void render() {
 		renderer.baseRender();
-	}
-
-	@Override
-	protected void resize(int width, int height) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void onKeyPress(int key, int mods) {
-		System.out.println("Key press: " + key);
-	}
-
-	@Override
-	public void onKeyDown(int key, int mods) {
-		System.out.println("Key down: " + key);
-	}
-	
-	@Override
-	public void dispose() {
-		super.dispose();
-		renderer.baseDispose();
 	}
 
 }
