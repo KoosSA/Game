719c8ca6f895ad9e12cbf6daeb5b1af0a1091879
KoosSA <kkposbus@gmail.com>
2023-05-28 16:26:48 +0200

    Moved material editor to a new runnable

---
+++ Game-Client/src/main/resources/client/MaterialEditor.java
@@ -0,0 +1,29 @@
+package client;
+
+import client.io.input.InputStates;
+import client.logic.BaseGameLoop;
+
+public class MaterialEditor extends BaseGameLoop {
+
+	public static void main(String[] args) {
+		new MaterialEditor().start();
+	}
+
+	@Override
+	protected void init() {
+		gui.loadXML("renderDemoUI.xml");
+		gui.show("renderDemoMainUI");
+		input.setInputReceiver(InputStates.GUI);
+	}
+
+	@Override
+	protected void update(float delta) {
+		
+	}
+
+	@Override
+	protected void render() {
+		
+	}
+
+}
