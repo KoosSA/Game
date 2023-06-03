b0f0302d047dc64126bb7459a2634d28d6a9cbd7
KoosSA <kkposbus@gmail.com>
2023-06-03 11:37:33 +0200

    Changed camera pos and pitch to look at origin

--- MaterialEditor/src/main/java/app/MaterialEditor.java
+++ MaterialEditor/src/main/java/app/MaterialEditor.java
@@ -3,6 +3,7 @@ package app;
 import app.renderers.MaterialRenderer;
 import client.io.input.InputStates;
 import client.logic.BaseGameLoop;
+import client.rendering.cameras.FirstPersonCamera;
 import client.utils.ResourceLoader;
 
 public class MaterialEditor extends BaseGameLoop {
@@ -18,9 +19,13 @@ public class MaterialEditor extends BaseGameLoop {
 		
 		gui.loadXML("renderDemoUI.xml");
 		gui.show("renderDemoMainUI");
+		
 		input.setInputReceiver(InputStates.GUI);
 		
 		renderer = new MaterialRenderer(camera);
+		
+		camera.getPosition().set(0, 2, 5);
+		FirstPersonCamera.class.cast(camera).pitch(-20);
 	}
 
 	@Override
