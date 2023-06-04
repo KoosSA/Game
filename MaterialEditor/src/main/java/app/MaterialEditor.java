e0b94cde2f7b35b4d6daa72851dc56b1e4ea1b14
KoosSA <kkposbus@gmail.com>
2023-06-04 21:33:40 +0200

    Set the default demo model to textured barrel for release

--- MaterialEditor/src/main/java/app/MaterialEditor.java
+++ MaterialEditor/src/main/java/app/MaterialEditor.java
@@ -4,6 +4,9 @@ import app.renderers.MaterialRenderer;
 import client.io.input.InputStates;
 import client.logic.BaseGameLoop;
 import client.rendering.cameras.FirstPersonCamera;
+import client.rendering.materials.Material;
+import client.rendering.materials.TextureType;
+import client.utils.Globals;
 import client.utils.ResourceLoader;
 import client.utils.registries.Registries;
 
@@ -18,15 +21,23 @@ public class MaterialEditor extends BaseGameLoop {
 		ResourceLoader.loadAllTextures();
 		ResourceLoader.loadAllModels();
 		
+		input.setInputReceiver(InputStates.GUI);
+		
 		gui.loadXML("renderDemoUI.xml");
 		gui.show("renderDemoMainUI");
-		
-		input.setInputReceiver(InputStates.GUI);
 		
 		renderer = new MaterialRenderer(camera);
 		
 		camera.getPosition().set(0, 2, 5);
 		FirstPersonCamera.class.cast(camera).pitch(-20);
+		
+		Material mat = Registries.Models.getStaticModel("barrels_fbx.fbx").getMeshes().get(0).getMaterial();
+		mat.addTexture(TextureType.BASE_COLOUR, "drum3_base_color.png");
+		mat.addTexture(TextureType.NORMAL, "drum3_normal.png");
+		mat.addTexture(TextureType.ROUGHNESS, "drum3_roughness.png");
+		mat.addTexture(TextureType.METALLIC, "drum3_metallic.png");
+		
+		MaterialRenderer.class.cast(renderer).setModel(Registries.Models.getStaticModel("barrels_fbx.fbx"));
 		
 	}
 
